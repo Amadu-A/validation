bf10b6e2722253902419d120658d11bb9a3de634
AmaduA <main_ivan@mail.ru>
2022-07-26 01:49:17 +0400

    dnt work validation

--- script.js
+++ script.js
@@ -31,9 +31,10 @@ fileInput.addEventListener('change', (e) => {
 
 });
 
-let validateForms = function(selector, rules, successModal, yaGoal) {
+let validateForms = function(selector, rules, messages, successModal, yaGoal) {
 	new window.JustValidate(selector, {
 		rules: rules,
+		messages: messages,
 		submitHandler: function(form) {
 			let formData = new FormData(form);
 
@@ -57,4 +58,35 @@ let validateForms = function(selector, rules, successModal, yaGoal) {
 	});
 }
 
-validateForms('.form', { email: {required: true, email: true}, tel: {required: true} }, '.thanks-popup', 'send goal');
+// validateForms('.form', { email: {required: true, email: true}, tel: {required: true} }, '.thanks-popup', 'send goal');
+let messages = {
+    name: {
+      required: "Вы не ввели имя",
+      minLength: "Имя должно иметь более 3 символов",
+      maxLength: "Имя должно быть менее 30 символов",
+    },
+    tel: {
+      required: "Неверный формат номера",
+      tel: "Введите телефон",
+    },
+}
+
+  validateForms('.form', {
+    rules: {
+      name: {
+        required: true,
+        minLength: 7,
+        maxLength: 30,
+      },
+      tel: {
+        tel: true,
+        function: (name, value) => {
+          const phone = document.querySelector('input[type="tel"]').inputmask.unmaskedvalue()
+          console.log(phone)
+          return Number(phone) && phone.length === 10
+        }
+      },
+	  email: {required: true, email: true},
+
+    },
+  }, messages, '.thanks-popup', 'send goal');
